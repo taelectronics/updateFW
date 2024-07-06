abb777a03e320c838d170acd9bc2dba3cee9d9f5
TruongLV <999vantruong@gmail.com>
2024-07-06 15:17:11 +0700

    create certificate key

--- UpdateFW/src/main.cpp
+++ UpdateFW/src/main.cpp
@@ -1,18 +1,50 @@
 #include <Arduino.h>
+#include <WiFiMulti.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include "HttpsOTAUpdate.h"
+
+#define WIFI_SSID "my wifi"
+#define WIFI_PASSSWORD  "12345566778"
+
+WiFiMulti wiFiMulti;
+
+/* put function declarations here: */
+void WiFi_Init();
+
+void Update_FW();
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  (void) WiFi_Init();
+  /**/
+
+  (void) Update_FW();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  /* put your main code here, to run repeatedly: */
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+void WiFi_Init()
+{
+  wiFiMulti.addAP(WIFI_SSID, WIFI_PASSSWORD);
+
+  while(wiFiMulti.run() != WL_CONNECTED)
+  {
+    delay(100);
+  }
+
+}
+
+
+void Update_FW()
+{
+  String url = "http://otadrive.com/deviceapi/update?";
+    url += "k=" + key;
+    url += "&v=" + version;
+    url += "&s=" + getChipId(); // định danh thiết bị trên Cloud
+
+    WiFiClient client;
+    httpUpdate.update(client, url, version);
 }
